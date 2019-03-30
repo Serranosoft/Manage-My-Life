0b12ee244303b35f1c79aa1c62c6258cb30bd0c0
Serranosoft <39860249+Serranosoft@users.noreply.github.com>
2019-03-30 20:45:01 +0100

    Diseño del menu lateral e implementación de los colores de la aplicación

--- app/src/main/java/com/example/manue/managemylife/Activities/MainActivity.java
+++ app/src/main/java/com/example/manue/managemylife/Activities/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        /*if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
@@ -94,7 +94,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
-        }
+        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
